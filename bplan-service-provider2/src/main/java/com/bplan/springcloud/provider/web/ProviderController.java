1fc53149fc7ba4feebdfc8ede897f4eebfc779e3
yh <yangheng@chk.com>
2017-04-23 16:49:13 +0800

    udpate

--- bplan-service-provider2/src/main/java/com/bplan/springcloud/provider/web/ProviderController.java
+++ bplan-service-provider2/src/main/java/com/bplan/springcloud/provider/web/ProviderController.java
@@ -3,6 +3,10 @@
  */
 package com.bplan.springcloud.provider.web;
 
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.joda.time.DateTimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/provider")
 public class ProviderController {
 	
-	  private static Logger logger = LoggerFactory.getLogger(ProviderController.class);
+	private static Logger logger = LoggerFactory.getLogger(ProviderController.class);
 	
 	@RequestMapping("/info")
     public String info(String name) {
 		logger.info("==== provider2 info ===={}",name);
-        return "hi "+name;
+        return "hi :"+name;
+    }
+	
+	@RequestMapping("/getName")
+    public String getName(String name) {
+		logger.info("==== provider2 getName ===={}",name);
+        return "The name is : "+name;
+    }
+	
+	@RequestMapping("/getCurrentTime")
+    public String getCurrentTime() {
+		Date date = new Date();
+		String strdate = DateFormat.getInstance().format(date);
+		logger.info("==== provider2 getCurrentTime ===={}",strdate);
+        return "This time is : "+strdate;
     }
 
 }
